26d6c679bb3e4bbd7bd3020b6a0799eedfe77205
fawda123 <mbafs2012@gmail.com>
2016-06-16 09:20:03 -0500

    floscl arg to dynaplot

--- R/globalVariables.R
+++
@@ -1,7 +0,0 @@
-globalVariables(c('.', 'day', 'flo', 'year', 'res', 'nrms_variable', 'nrms_value', 'fits_variable',
-  'fits_value', 'bt_norm', 'bt_fits', 'lnres', 'lnQ', 'value', 'taus', 'month', 'i', 'tidfit',
-  'rsdnl', 'mod', 'Year', 'mo', 'fake_date', 'flo.x', 'flo.y', 'nrm', 'fit', 'year_dum', 'month2',
-  'yrcat', 'mocat', 'matches', 'yr', 'yrchg', 'val', 'xvar', 'one_of'))
-
-#' @importFrom stats AIC approx arima.sim ave coef constrOptim fitted lm median na.omit nobs optim predict quantile resid rnorm sd var
-NULL
