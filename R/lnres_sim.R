07678499a668a14f04fb309b5610f82c5510180f
fawda123 <mbafs2012@gmail.com>
2016-09-08 15:39:06 -0500

    lnres_seas seasonal model added to output for lnres_sim

--- R/lnres_sim.R
+++ R/lnres_sim.R
@@ -7,7 +7,7 @@
 #' 
 #' @details This function creates a simulated water quality time series and requires error estimates for an observed water quality dataset and a simulated discharge time series.  The water quality time series is created as the additive combination of a seasonal, stationary time component (as in \code{\link{lnQ_sim}} for discharge), a random error component from \code{\link{lnres_err}}, and a simulated discharge time series from \code{\link{lnQ_sim}}.  The discharge time series is considered an explicit component of the water quality time series and is first centered at zero prior to adding.  The optional vector of coefficients passed to \code{lnQ_coef} can mediate the influence of discharge on the water quality time series.  For example, a vector of all zeroes implies no effect, whereas a vector of all ones implies a constant effect (default).
 #' 
-#' @return The original data frame with additional columns for a flow-independent water quality time series (\code{lnres_noQ}) and a flow-dependent time series (\code{lnres_Q}).
+#' @return The original data frame with additional columns for the seasonal water quality model (\code{lnres_seas}), a flow-independent water quality time series (\code{lnres_noQ}), and a flow-dependent time series (\code{lnres_Q}).
 #' 
 #' @export
 #' 
@@ -38,13 +38,14 @@ lnres_sim <- function(dat_in, lnQ_coef = NULL){
   if(is.null(lnQ_coef)) lnQ_coef <- rep(1, length = nrow(dat_in))
   
   # seasonal wq component, no discharge
-  lnres_noQ <- lm(lnres ~ dec_time + sin(2 * pi * dec_time) + cos(2 * pi * dec_time), 
+  lnres_seas <- lm(lnres ~ dec_time + sin(2 * pi * dec_time) + cos(2 * pi * dec_time), 
     data = dat_in)
-  lnres_noQ <- predict(lnres_noQ) + with(dat_in, scls * errs)
+  lnres_noQ <- predict(lnres_seas) + with(dat_in, scls * errs)
   
   # add discharge, rescale 
   lnres_Q <- as.numeric(lnres_noQ + with(dat_in, lnQ_coef * scale(lnQ_sim, scale = FALSE)))
   
+  dat_in$lnres_seas <- predict(lnres_seas)
   dat_in$lnres_noQ <- lnres_noQ
   dat_in$lnres_Q <- lnres_Q
   
