1ff0692bc8189acaa955edfe6a27f63b37d14b90
fawda123 <mbafs2012@gmail.com>
2016-09-12 11:41:40 -0500

    more flexible version of samp_sim

--- R/samp_sim.R
+++ R/samp_sim.R
@@ -3,13 +3,16 @@
 #' Sample a daily water quality time series at a set monthly frequency
 #'
 #' @param dat_in input \code{\link[base]{data.frame}} that is returned from \code{\link{lnres_sim}} or \code{\link{all_sims}}
-#' @param month_samps numeric indicating sampling interval, defaults to once a month and is random within the month.  Multiple samples per month are based on sampling with replacement. 
+#' @param unit chr string indicating sampling unit, must be year, quarter, month, or week for equivalent lubridate function
+#' @param irregular logical indicating if monthly sampling is done randomly within each \code{unit}, otherwise the first value is returned
 #' 
 #' @details This function is intended for sampling a simulated daily time series of water quality that is returned by \code{\link{lnres_sim}} or \code{\link{all_sims}}.
 #' 
 #' @return Original data frame with rows subset based on number of desired monthly samples.
 #' 
 #' @export
+#' 
+#' @import dplyr lubridate
 #' 
 #' @seealso \code{\link{lnres_sim}}, \code{\link{all_sims}}
 #' 
@@ -24,22 +27,24 @@
 #' ## sample
 #' samp_sim(tosamp)
 #' }
-samp_sim <- function(dat_in, month_samps = 1){
- 
-  dat_in$mos <- strftime(dat_in$date, '%m')
+samp_sim <- function(dat_in, unit = 'month', irregular = TRUE){
 
-  splits <- split(dat_in, dat_in[, c('year', 'mos')])
+  # sanity check
+  if(!unit %in% c('year', 'quarter', 'month', 'week'))
+    stop('unit must year, quarter, month, or week')
   
-  sels <- lapply(splits, function(x){
-    tosel <- sample(1:nrow(x), size = month_samps, replace = TRUE)
-    x[tosel, ]
-  })
-
-  sels <- do.call('rbind', sels)
-  sels <- sels[order(sels$date), ]
-  row.names(sels) <- 1:nrow(sels)
-  sels$mos <- NULL
-
-  return(sels)
+  # get sampling unit
+  uni_str <- paste0(unit, '(dat_in$date)')
+  unit <- eval(parse(text = uni_str))
+  
+  # get indices in dat_in for sampling
+  inds <- data.frame(inds = 1:nrow(dat_in), year = dat_in$year, unit = unit) %>% 
+    group_by(year, unit) %>% 
+    summarize(inds = ifelse(irregular, sample(inds, 1), inds[1])) %>% 
+    .$inds
+  
+  # output
+  out <- dat_in[inds, ]
+  return(out)
    
 }
